122293e2452c380a190059f19e7d2109de6c1d99
Santhanam Elumalai <selumalai@opentable.com>
2022-02-20 12:42:58 -0700

    route update

--- server/app.js
+++ server/app.js
@@ -12,9 +12,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './public')));
 
 app.use('/', usersRouter);
 app.use('/users', usersRouter);
+
 
 export default app;
