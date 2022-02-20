71ba7dbc2555dd503e264698a59c342ddc0c5086
Santhanam Elumalai <selumalai@opentable.com>
2022-02-20 13:21:45 -0700

    update route with try catch

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,11 +9,11 @@ import App from '../../client/app';
 
 const router = express.Router();
 
-// const renderRequestPage = async (pageName) => {
-//   const pagePath = path.resolve(`./dist/public/${pageName}.html`);
-//   const requestPage = await fs.promises.readFile(pagePath, 'utf8');
-//   return requestPage;
-// };
+const renderRequestPage = async (pageName) => {
+  const pagePath = path.resolve(`./dist/public/${pageName}.html`);
+  const requestPage = await fs.promises.readFile(pagePath, 'utf8');
+  return requestPage;
+};
 
 const BuildAppComponentString = (store) => ReactDOMServer.renderToString(
   <Provider store={store}>
@@ -23,13 +23,17 @@ const BuildAppComponentString = (store) => ReactDOMServer.renderToString(
 
 /* GET home page. */
 router.get('/', async (req, res) => {
-  //const requestPage = await renderRequestPage('index');
+  try{
+  const requestPage = await renderRequestPage('index');
   const store = buildStore({ counter: { value: 10 } });
   const appString = BuildAppComponentString(store);
-  // const resData = requestPage
-  //   .replace('<div id="root"></div>', `<div id="root">${appString}</div>`)
-  //   .replace('__PRELOADED_STATE_DATA', JSON.stringify(store.getState()));
-  res.send(appString);
+  const resData = requestPage
+    .replace('<div id="root"></div>', `<div id="root">${appString}</div>`)
+    .replace('__PRELOADED_STATE_DATA', JSON.stringify(store.getState()));
+  res.send(resData);
+  }catch(e){
+    res.send(e);
+  }
 });
 
 export default router;
